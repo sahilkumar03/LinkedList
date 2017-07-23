d8743dbd27e81527d07874a45d4b17c2bf3ef69a
sahilkumar03 <sahilkumar_03@Outlook.com>
2017-07-23 13:02:34 +0530

    Update linklist.c

--- linklist.c
+++ linklist.c
@@ -1,147 +1,4 @@
     #include <stdio.h>
-    #include<stdlib.h>
-    #include<stdbool.h>
-    struct node{
-      int data;
-      struct node *next;
-    };
-    void insert(int data,struct node **head_ptr){
-        if(*head_ptr==NULL)
-        {
-            struct node *temp=(struct node*)malloc(sizeof(struct node));
-            temp->data=data;
-            temp->next=NULL;
-            *head_ptr=temp; 
-             //printf("\ninserted data=%d successfully",temp->data);
-
-        }
-        else
-        {
-            struct node * temp=(struct node*)malloc(sizeof(struct node));
-            temp->data=data;
-            temp->next=*head_ptr;
-            *head_ptr=temp;
-            //printf("\ninserted data=%d successfully",temp->data);
-        }
-    }
-    
-    void insertAt(int pos,int data,struct node *head,struct node **head_ptr){
-   
-     int count=1;
-        struct node *prev=head;
-        
-        while(prev!=NULL){
-           
-            if(count == pos)
-            {
-                struct node *temp=(struct node*)malloc(sizeof(struct node));
-                temp->data=data;
-                
-                temp->next=head;
-                if(count==1)
-                { 
-                    *head_ptr=temp;
-                    break;
-                }
-                 prev->next=temp;
-                
-            }
-            
-             prev=head;
-            head=head->next;     
-            count++;
-            if(count==pos&&head==NULL){
-                struct node *temp=(struct node*)malloc(sizeof(struct node));
-                temp->data=data;
-                temp->next=NULL;
-                prev->next=temp;
-                break;
-            }
-        }   
-        
-    }
-
-    void print(struct node* head){
-        int count=1;
-        struct node *temp=head;
-        while(temp!=NULL){
-            printf("\nnode-%d data=%d\n",count,temp->data);
-            count++;
-            temp=temp->next;
-        }
-    }
-    
-    void delete(int key,struct node** head_ptr)
-    {
-        struct node *head;
-        head=(*head_ptr);
-        struct node *prev=NULL; 
-          while(head!=NULL)
-          {
-              
-              if(head->data==key)
-              {
-                  if(prev==NULL)
-                  {
-                      *head_ptr=head->next;
-                      
-                      break;
-                  }
-                  prev->next=head->next;
-                  free(head);
-                  break;
-              }
-              prev=head;
-              head=head->next;
-          }
-    }
-    
-    
-    int deleteAt(int pos,struct node **head_ptr){
-        struct node *head;
-        struct node *prev=NULL;
-        head=*head_ptr;
-        
-        while(pos>1)
-        {
-            prev=head;
-            head=head->next;
-            pos--;
-        }
-        
-                  if(prev==NULL)
-                  {
-                      *head_ptr=head->next;
-                  }
-                  else
-                    prev->next=head->next;
-        free(head);
-        int x=head->data;
-        return x;
-    }
-    
-    
-    
-    
-    
-    int main()
-    {      
-        struct node *head=NULL;
-        insert(1,&head);
-        insert(8,&head);
-        insertAt(3,3,head,&head);
-     
-        deleteAt(1,&head);
-     
-     
-        print(head);
-        return 0;
-    }
-    
-
-
-
-    #include <stdio.h>
    // #include <conio.h>
     #include<stdlib.h>
     #include<stdbool.h>
@@ -294,6 +151,21 @@
     }
     
     
+    int printLength(struct node *head){
+    	int count=0;
+    	while(head!=NULL)
+    	{
+    		head=head->next;
+    		count++;
+    	}
+    return count;
+    }
+    
+    int printLengthR(int n,struct node *head){
+    	if(head==NULL)
+    	return n;
+    return printLengthR(n++,head);
+    }
     
     
     int main()
@@ -308,12 +180,17 @@
         insert(2,&head);
         insert(1,&head);
 //        insertAt(6,6,head,&head);
-     
     //    deleteAt(1,&head);
-        print(head);
-        findendnthnode(4,&head);
+    
+        //print(head);
+    	printf("linked list length = %d",printLength(head));
+    	printf("linked list length = %d",printLengthR(0,head));
+
+      //  findendnthnode(4,&head);
+
         return 0;
     }
+    
     
 
 
